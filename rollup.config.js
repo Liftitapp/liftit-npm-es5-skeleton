97039916f93fe55a819fe5bf1341a3a5605036ae
Angel Botto <angelbotto@gmail.com>
2017-07-26 01:58:05 -0500

    build system

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import uglify from 'rollup-plugin-uglify'
+
+export default {
+  entry: 'src/index.js',
+  format: 'umd',
+  exports: 'named',
+  moduleName: 'liftitSkeleton',
+  dest: 'dist/liftit-skeleton.js',
+  plugins: [
+    babel({
+      babelrc: false,
+      presets: [
+        [
+          'es2015',
+          {
+            modules: false
+          }
+        ]
+      ]
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    nodeResolve({
+      jsnext: true,
+      main: true,
+      browser: true
+    }),
+    uglify()
+  ]
+}
